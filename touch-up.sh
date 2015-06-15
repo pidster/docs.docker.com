452190d56e515dc8f994d470b75601a76af588ca
Mary Anthony <mary@docker.com>
2015-06-15 12:38:44 -0700

    Expanding the sed to markdown links
    Signed-off-by: Mary Anthony <mary@docker.com>

--- touch-up.sh
+++ touch-up.sh
@@ -4,8 +4,29 @@ DOCS_DIR=$( dirname $( find /docs -name 'build.json' | head -n1 ) )
 BUILD_JSON="${DOCS_DIR}/build.json"
 BUILDINFO_PARTIAL="${DOCS_DIR}/layouts/partials/buildinfo.html"
 
-# Uncomment hugo metadata
-find /docs -type f -name '*.md' -exec sed -i.old  -e '/^<!.*metadata]>/g' -e '/^<!.*end-metadata.*>/g' {} \;
+# Sed to process GitHub Markdown
+# 1-2 Remove comment code from metadata block
+# 3 Remove .md extension from link text
+# 4 Change ](/ to ](/project/ in links
+# 5 Change ](word) to ](/project/word)
+# 6 Change ](../../ to ](/project/
+# 7 Change ](../ to ](/project/word)
+# 
+for i in ls -l /docs/content/*
+  do                 # Line breaks are important
+    if [ -d $i ]   # Spaces are important
+      then
+        y=${i##*/}
+        find $i -type f -name "*.md" -exec sed -i.old \
+            -e '/^<!.*metadata]>/g' \
+            -e '/^<!.*end-metadata.*>/g' \
+            -e 's/\([(]\)\(.*\)\(\.md\)/\1\2/g' \
+            -e 's/\(\]\)\([(]\)\(\/\)/\1\2\/'$y'\//g' \
+            -e 's/\(\][(]\)\([A-z]*[)]\)/\]\(\/'$y'\/\2/g' \
+            -e 's/\(\][(]\)\(\.\.\/\)/\1\/'$y'\//g' {} \;    
+      fi
+done
+
 
 # Substitute in the build data in the buildinfo partial
 sed "/BUILD_DATA/r $BUILD_JSON" "$BUILDINFO_PARTIAL" | sed '/BUILD_DATA/d' > "${BUILDINFO_PARTIAL}.out" \
