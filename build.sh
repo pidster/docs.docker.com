d4da9ed71093a23da3bcae2730275a787969dae3
Mary Anthony <mary@docker.com>
2015-11-03 14:47:57 -0800

    Updating for release 1.9
    Signed-off-by: Mary Anthony <mary@docker.com>

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -e
 
 # TODO: need to add the $DOCS_VERSION to the baseURL without duplicating the /
-hugo -d /public/$DOCS_VERSION --config=config.toml --baseURL="$S3HOSTNAME"
+hugo -d /public/$DOCS_VERSION --config=config.toml --baseURL="http://$S3HOSTNAME"
